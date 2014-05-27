d24b8cccaa1d231fbaf6c068d3bb46f728cf0c23
cetoli <carlo@ufrj.br>
2014-05-27 13:37:55 -0300

    com diversos naipes

--- src/vista.py
+++ src/vista.py
@@ -1,17 +1,22 @@
 __author__ = 'cetoli'
-FACE = 'http://thumbs.dreamstime.com/z/joker-playing-card-19231679.jpg'
-CH = CV = 50
-
+#FACE = 'http://upload.wikimedia.org/wikipedia/commons/9/9b/Poker-sm-212-Ks.png'
+FACE = 'http://upload.wikimedia.org/wikipedia/commons/3/36/Playing_card_club_%s.svg'
+CARDS = 'ace 2 3 4 5 6 7 8 9 10 jack2 queen2 king2'.split()
+CH = CV = 80
+FACESVG = 'http://upload.wikimedia.org/wikipedia/commons/7/78/Contemporary_playing_cards.svg'
+FACE = "/lib/cards/%s_of_%s.svg"
+NAIPES = "clubs hearts spades diamonds".split()
 
 class Carta:
-    def __init__(self, html, xy, deque):
+    def __init__(self, html, xy, deque, face=FACE % ('ace', 'clubs')):
         x, y = self.pos = xy
         self.deque = deque
-        ct = self.e_carta = html.IMG(src=FACE, width=CH, heigth=CV)
+        ct = self.e_carta = html.IMG(src=face, width=CH, heigth=CV)
         ct.style.position = "absolute"
         ct.style.left, ct.style.top = xy
-        x = x / 5
-        ct.style.transition = "left 2s linear %fs, top 2s linear %fs" % (x, x)
+        x /= 8
+        d = 0.6
+        ct.style.transition = "left %fs linear %fs, top %fs linear %fs" % (d, x, d, x)
         deque <= ct
         ct.onclick = self.voa
 
@@ -29,10 +34,12 @@ class Carta:
 class Deque:
     def __init__(self, html, tela):
         self.tela = tela
-        self.deque = [Carta(html, (x*4, 10), self) for x in range(10)]
+        self.deque = [
+            Carta(html, ((n*13+x)*4, 10), self, FACE % (CARDS[x], naipe))
+            for n, naipe in enumerate(NAIPES) for x in range(13)]
 
     def voa(self):
-        [carta.voar((100, 100)) for carta in self.deque]
+        [carta.voar((100, 100)) for carta in self.deque[::-1]]
 
     def __le__(self, other):
         self.tela <= other
